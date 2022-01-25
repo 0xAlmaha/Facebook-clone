2532539225b940dbe186a21292486c14c55a0312
Almaha <97455971+0xAlmaha@users.noreply.github.com>
2022-01-25 20:34:53 +0300

    Delete db directory

--- db/migrate/20220124223449_update_users_table.rb
+++
@@ -1,6 +0,0 @@
-class UpdateUsersTable < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :phone_number, :number
-    #updating the user table and adding a phone number column
-  end
-end
--- db/migrate/20220125013909_update_posts_table.rb
+++
@@ -1,5 +0,0 @@
-class UpdatePostsTable < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :status, :string
-  end
-end
